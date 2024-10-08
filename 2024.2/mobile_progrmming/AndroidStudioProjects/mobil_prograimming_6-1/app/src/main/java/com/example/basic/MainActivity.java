56440c2b9aac4f9c67f81f0fd7543bdbc5e1a0b1
hyeonu <hyeonu9831@naver.com>
2024-10-08 12:06:16 +0900

    dadf

--- 2024.2/mobile_progrmming/AndroidStudioProjects/mobil_prograimming_6-1/app/src/main/java/com/example/basic/MainActivity.java
+++ 2024.2/mobile_progrmming/AndroidStudioProjects/mobil_prograimming_6-1/app/src/main/java/com/example/basic/MainActivity.java
@@ -15,12 +15,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
-    Button btn2;
-    Button btn3;
-    Button btn4;
-    Button btn5;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,45 +22,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main); //UI 연걸
 
-        //xml에서 생성한 버튼이랑 연결
-        btn1 =(Button) findViewById(R.id.button1);
-        btn2 =(Button) findViewById(R.id.button2);
-        btn3 =(Button) findViewById(R.id.button3);
-//        btn4 =(Button) findViewById(R.id.button4);
-//        btn5 =(Button) findViewById(R.id.button5);
 
-        btn1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Toast.makeText(getApplicationContext(), "20213348-서현우", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                finish();
-            }
-        });
-        btn3.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                String url = "http://www.naver.com";
-                Intent naver = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(naver);
-            }
-        });
-        btn4.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                btn4.setBackgroundColor(Color.GRAY);
-            }
-        });
-        btn5.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                btn5.setTextColor(Color.GRAY);
-            }
-        });
     }
 }
