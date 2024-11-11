85b18b014f5073724c7fde5751d614e5db975c1b
hyeonu <hyeonu@hyeonuui-MacBookAir.local>
2024-11-11 16:40:05 +0900

    Update MainActivity and activity_main layout

--- 2024.2/mobile_progrmming/Mp_chapter10/app/src/main/java/com/example/mp_chapter10/MainActivity.java
+++ 2024.2/mobile_progrmming/Mp_chapter10/app/src/main/java/com/example/mp_chapter10/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.mp_chapter10;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> data = Arrays.asList("Item 1", "Item 2","Item 3","Item 4","Item 5");
+
+        MyAdapter adapter = new MyAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
