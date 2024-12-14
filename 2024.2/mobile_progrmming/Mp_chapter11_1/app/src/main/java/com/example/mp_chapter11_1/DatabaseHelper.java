bdb8dcfdda2df80ae3cc0ece884898b9e311f1c4
hyeonu <hyeonu9831@gmail.com>
2024-12-14 20:20:57 +0900

    12.14

---
+++ 2024.2/mobile_progrmming/Mp_chapter11_1/app/src/main/java/com/example/mp_chapter11_1/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.mp_chapter11_1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "student.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "students";
+    private static final String COL_ID = "_id";
+    private static final String COL_NAME = "name";
+    private static final String COL_STUDENT_ID = "student_id";
+
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_NAME + " TEXT, " +
+                COL_STUDENT_ID + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void insertData(String name, String studentId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, name);
+        values.put(COL_STUDENT_ID, studentId);
+        db.insert(TABLE_NAME, null, values);
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+
+    public void updateData(long id, String name, String studentId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, name);
+        values.put(COL_STUDENT_ID, studentId);
+        db.update(TABLE_NAME, values, COL_ID + " = ?", new String[]{String.valueOf(id)});
+    }
+
+    public void deleteData(long id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, COL_ID + " = ?", new String[]{String.valueOf(id)});
+    }
+}
---
+++ 2024.2/mobile_progrmming/Mp_chapter11_1/app/src/main/java/com/example/mp_chapter11_1/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.mp_chapter11_1;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextId;
+    private Button buttonDelete;
+    private Button buttonUpdate;
+    private ListView listView;
+    private DatabaseHelper dbHelper;
+    private long selectedId = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextId = findViewById(R.id.editTextId);
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonUpdate = findViewById(R.id.buttonUpdate);
+        listView = findViewById(R.id.listView);
+
+        dbHelper = new DatabaseHelper(this);
+
+        // 등록 버튼 클릭 이벤트
+        buttonRegister.setOnClickListener(v -> {
+            String name = editTextName.getText().toString();
+            String studentId = editTextId.getText().toString();
+            if (!name.isEmpty() && !studentId.isEmpty()) {
+                dbHelper.insertData(name, studentId);
+                loadData();
+                clearInputs();
+            }
+        });
+
+        // 수정 버튼 클릭 이벤트
+        buttonUpdate.setOnClickListener(v -> {
+            String name = editTextName.getText().toString();
+            String studentId = editTextId.getText().toString();
+            if (selectedId != -1 && !name.isEmpty() && !studentId.isEmpty()) {
+                dbHelper.updateData(selectedId, name, studentId);
+                loadData();
+                clearInputs();
+            }
+        });
+
+        // 삭제 버튼 클릭 이벤트
+        buttonDelete.setOnClickListener(v -> {
+            if (selectedId != -1) {
+                dbHelper.deleteData(selectedId);
+                loadData();
+                clearInputs();
+            }
+        });
+
+        // 리스트뷰 아이템 클릭 이벤트
+        listView.setOnItemClickListener((adapterView, view, position, id) -> {
+            Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+            selectedId = cursor.getLong(cursor.getColumnIndex("_id"));
+            editTextName.setText(cursor.getString(cursor.getColumnIndex("name")));
+            editTextId.setText(cursor.getString(cursor.getColumnIndex("student_id")));
+            buttonDelete.setEnabled(true);
+            buttonUpdate.setEnabled(true);
+        });
+
+        loadData();
+    }
+
+    private void loadData() {
+        Cursor cursor = dbHelper.getData();
+        String[] from = {"_id", "name", "student_id"};
+        int[] to = {R.id.textViewIndex, R.id.textViewName, R.id.textViewId};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_item, cursor, from, to, 0);
+        listView.setAdapter(adapter);
+    }
+
+    private void clearInputs() {
+        editTextName.setText("");
+        editTextId.setText("");
+        selectedId = -1;
+        buttonDelete.setEnabled(false);
+        buttonUpdate.setEnabled(false);
+    }
+
+}
